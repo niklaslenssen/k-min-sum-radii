c41671ebb7b62cea9ba8f4a7fc2d4f828d5d5724
Niklas Lenßen <niklas.lenssen@outlook.de>
2024-04-24 15:06:59 +0200

    updated generator for clusters

--- generator.py
+++ generator.py
@@ -6,10 +6,39 @@ from sklearn.datasets import make_blobs
 
 from hochbaumShmyos import hochbaum_shmoys_k_center_from_csv
 
+# Anzahl der Punkte
+n_samples = 30
+
+# Anzahl der Cluster
 k = 3
 
-X, y = make_blobs(n_samples=100, centers=k, n_features=2, cluster_std=0.8, random_state=1) # type: ignore
+# Dimension der Punkte
+n_features = 2
 
+# Standardabweichung der Punkte zu ihren Centren
+cluster_std_global = 0.3
+
+# Bereich der Centren
+center_box = (0, 8)
+
+# Seed
+random_state = 20
+
+# Generiere Daten mit einer globalen Standardabweichung.
+X, y = make_blobs(n_samples=n_samples, centers=k, n_features=n_features,
+                  cluster_std=cluster_std_global, center_box=center_box, random_state=random_state)
+
+# Individuelles Rauschen für jeden Cluster definieren.
+cluster_stds = [0.3, 0.1, 0.5]
+
+# Füge unterschiedliches Rauschen zu jedem Cluster hinzu.
+for i in range(k):
+    cluster_points = X[y == i]
+    adjusted_noise = np.random.normal(0, cluster_stds[i], cluster_points.shape)
+    X[y == i] = cluster_points + adjusted_noise
+
+
+# Speichern der Punkte in einer CSV Datei.
 df = pd.DataFrame(X, columns=['x', 'y'])
 df['Center'] = y
 
