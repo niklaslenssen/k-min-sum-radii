adf3a1ef96d1f5ad07d610f5d542667df2cf5c3b
Niklas Lenßen <niklas.lenssen@outlook.de>
2024-04-24 14:42:34 +0200

    added distance function

--- header/Point.h
+++ header/Point.h
@@ -14,16 +14,21 @@ class Point {
   Point() {}
   Point(std::vector<double> coords) : coordinates(coords) {}
 
+  // Euklidischer Abstand zwischen 'this' und einem anderen Punkt.
   double distanceTo(const Point& other) const {
     if (coordinates.size() != other.coordinates.size()) {
       throw std::invalid_argument("Die Punkte müssen dieselbe Dimension haben!");
     }
     double sum = 0.0;
-    for (int i = 0; i < coordinates.size(); ++i) {
-      sum += (coordinates[i] - other.coordinates[i]) *
-             (coordinates[i] - other.coordinates[i]);
+    for (int i = 0; i < coordinates.size(); i++) {
+      sum += (coordinates[i] - other.coordinates[i]) * (coordinates[i] - other.coordinates[i]);
     }
     return sqrt(sum);
+  }
+
+  // Euklidischer Abstand zwischen zwei Punkten.
+  static double distance(const Point& p1, const Point& p2) {
+    return p1.distanceTo(p2);
   }
 
   bool operator<(const Point& other) const {
