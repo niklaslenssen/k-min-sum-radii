5a4021ac99f6e86ed0f4c60635c99d0ea5b97d74
Niklas Lenßen <niklas.lenssen@outlook.de>
2024-04-29 11:31:44 +0200

    added first evaluation skript

--- eval.py
+++ eval.py
@@ -1,14 +1,16 @@
 import SHK.Algorithms as Algorithms
 import SHK.RandomCenters as RandomCenters
+import SHK.Points as Points
 import subprocess
 import plot
+import matplotlib.pyplot as plt
 
 def main():
-    k = 3
+    config = RandomCenters.handle_arguments()
+    k = config['number_centers']
     epsilon = 0.5
 
-    config = RandomCenters.handle_arguments()
-    print(f"Generating cluster data with the following config:\n{config}")
+    
     for _ in range(config["number_files"]):
         centers, points = RandomCenters.generate_clusters(config)
         if config["only_print"]:
@@ -20,21 +22,38 @@ def main():
 
     filename = "Data/points.csv"
 
-    # subprocess.run(['g++', '-fopenmp', '-o', 'main', 'main.cpp', 'k_MSR.cpp', 'badoiu_clarkson.cpp', 'welzl.cpp', 'hochbaumShmyos.cpp' ])
+    imageOutputSimpleGonzalez = f"Data/Plots/PlainGonzalez.png"
+    imageOutputWithoutMerge = f"Data/Plots/WithoutMerge.png"
+    imageOutputWithMerge = f"Data/Plots/WithMerge.png"
+    imageOutputWithMergeIterative = f"Data/Plots/WithMergeIterative.png"
+
+    subprocess.run(['g++', '-fopenmp', '-o', 'main', 'main.cpp', 'k_MSR.cpp', 'badoiu_clarkson.cpp', 'welzl.cpp', 'hochbaumShmyos.cpp' ])
     subprocess.run(['./main', f'{k}', f'{epsilon}'])
-
-
     plot.plot_cluster()
 
+    points = Points.read_points(filename)
 
+    _, clustered_points, rad_sum = Algorithms.gonzalez_k_msr(points, k)
+    radii = Algorithms.get_radii_of_clustering(clustered_points, k)
+    Points.save_plot(imageOutputSimpleGonzalez, clustered_points, showRadii=True, radii=radii)
+    print(f"Gonzales:                  {round(rad_sum, 6)}")
+
+    _, clustered_points, rad_sum = Algorithms.iterative_gonzalez_k_msr(points, k)
+    radii = Algorithms.get_radii_of_clustering(clustered_points, k)
+    Points.save_plot(imageOutputWithoutMerge, clustered_points, showRadii=True, radii=radii)
+    print(f"Gonzales iterativ:         {round(rad_sum, 6)}")
+
+    _, clustered_points, rad_sum = Algorithms.merging_k_msr(points, k, use_iterative_gonzalez=False)
+    radii = Algorithms.get_radii_of_clustering(clustered_points, k)
+    Points.save_plot(imageOutputWithMerge, clustered_points, showRadii=True, radii=radii)
+    print(f"Merging Gonzales:          {round(rad_sum, 6)}")
+
+    _, clustered_points, rad_sum = Algorithms.merging_k_msr(points, k)
+    radii = Algorithms.get_radii_of_clustering(clustered_points, k)
+    Points.save_plot(imageOutputWithMergeIterative, clustered_points, showRadii=True, radii=radii)
+    print(f"Merging Gonzales iterativ: {round(rad_sum, 6)}")
 
     return
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
