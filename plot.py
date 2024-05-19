2181b21198ec441f71ce6e17b8e372e440168097
Niklas Lenßen <niklas.lenssen@outlook.de>
2024-05-19 13:36:22 +0200

    modified plot settings

--- plot.py
+++ plot.py
@@ -2,9 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 
-def plot_cluster():
-    points = pd.read_csv('Data/cluster.csv', header=None, names=['x', 'y', 'Center'])
-    balls = pd.read_csv('Data/balls.csv', header=None, names=['x', 'y', 'radius'])
+def plot_cluster(cluster_file, ball_file, plot_file):
+
+    plt.close('all')
+
+    plt.clf()
+
+    points = pd.read_csv(cluster_file, header=None, names=['x', 'y', 'Center'])
+    balls = pd.read_csv(ball_file, header=None, names=['x', 'y', 'radius'])
 
     plt.scatter(points['x'], points['y'], c=points['Center'])
     ax = plt.gca()
@@ -16,4 +21,4 @@ def plot_cluster():
 
 
     plt.axis('equal')
-    plt.savefig('Data/Plots/schmidt.png')
+    plt.savefig(plot_file)
